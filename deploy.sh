d63d358c23ad086dfb653fe7fe026bf614f24f2b
nobody <ops.liu1116@gmail.com>
2013-04-27 15:03:04 -0400

    refactor deploy script.

--- deploy.sh
+++ deploy.sh
@@ -6,21 +6,22 @@ DEPLOY_USER=`who -m | cut -d " " -f 1`
 
 SOURCE_PATH=$(cd $(dirname $0); pwd -P)
 TARGET_PATH="/var/www/html"
+BACKUP_FOLDER="/var/www/html_bak"
 
 echo "stop Apache..."
 sudo /etc/init.d/httpd stop
 
-echo "backup running code: REV: $CURRENT_REVISION ..."
-sudo rm -rf "$TARGET_PATH/bbs_old"
-sudo cp -r "$TARGET_PATH/bbs15eng" "$TARGET_PATH/bbs_old"
+echo "backup running cache..."
+sudo rm -rf "$BACKUP_FOLDER"
+sudo cp -r "$TARGET_PATH" "$BACKUP_FOLDER" 
 
 echo "pull latest code..."
 sudo git pull
 DEPLOY_REV=`git log | grep ^commit | head -1 | cut -d ' ' -f 2`
 
 echo "deploy new branch: $BRANCH, REV: $DEPLOY_REV ..."
-cd "$SOURCE_PATH/bbs15eng"
-sudo cp -r "*" "$TARGET_PATH/" 
+cd "$SOURCE_PATH/bbs15eng/upload"
+sudo cp -r * "$TARGET_PATH/" 
 cd "$SOURCE_PATH"
 
 echo "start Apache..."
