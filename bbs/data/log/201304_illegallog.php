091a50c95fd0955afba32e31f6b184a6c524bbf9
nobody <ops.liu1116@gmail.com>
2013-04-26 22:53:47 -0400

    delete all

--- bbs/data/log/201304_illegallog.php
+++
@@ -1,7 +0,0 @@
-<?PHP exit;?>	1366253725	admin	say***ng	Ques #0	216.165.200.107
-<?PHP exit;?>	1366253733	admin	say***ng	Ques #0	216.165.200.107
-<?PHP exit;?>	1366253819	admin	say***ng	Ques #0	216.165.200.107
-<?PHP exit;?>	1366256472	admin	say***ng	Ques #0	216.165.200.107
-<?PHP exit;?>	1366256574	root	say***ng	Ques #0	216.165.200.107
-<?PHP exit;?>	1366256662	admin	say***ng	Ques #0	216.165.200.107
-<?PHP exit;?>	1366299976	root	say***ng	Ques #0	206.214.242.146
