b87dfb1788688177bca7d16485398076d51fcec9
nobody <ops.liu1116@gmail.com>
2013-04-28 16:53:28 -0400

    disclaimer updated, env deploy supported.

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
 
+function usage {
+  echo "./deploy.sh [-h] -e <dev|prod>"
+  exit 0
+}
+
+while getopts "he:m:" OPTIONS
+  do
+    case $OPTIONS
+    in 
+    h)
+    usage
+    ;;
+    e)
+    DEPLOY_ENV=$OPTARG
+    ;;
+    *)
+    usage
+    ;;
+    esac
+  done
+
+if [  -z "$DEPLOY_ENV" ]; then
+  usage
+fi
+
+echo "ready to start..."
+
 BRANCH=`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
 CURRENT_REVISION=`git log --pretty=format:"%h" -n1`
 DEPLOY_USER=`who -m | cut -d " " -f 1`
 
 SOURCE_PATH=$(cd $(dirname $0); pwd -P)
-TARGET_PATH="/var/www/html"
-BACKUP_FOLDER="/var/www/html_bak"
+if [ "$DEPLOY_ENV" == "prod" ];then
+  TARGET_PATH="/var/www/html"
+  BACKUP_FOLDER="/var/www/html_bak"
+else
+  TARGET_PATH="/Users/kelinliu/Sites/escortreviewed"
+  BACKUP_FOLDER="/Users/kelinliu/Sites/escortreviewed_bak"
+fi
 
 echo "stop Apache..."
-sudo /etc/init.d/httpd stop
+if [ "$DEPLOY_ENV" == "prod" ];then
+  sudo /etc/init.d/httpd stop
+fi
 
 echo "backup running cache..."
 sudo rm -rf "$BACKUP_FOLDER"
@@ -19,12 +53,14 @@ echo "pull latest code..."
 sudo git pull
 DEPLOY_REV=`git log | grep ^commit | head -1 | cut -d ' ' -f 2`
 
-echo "deploy new branch: $BRANCH, REV: $DEPLOY_REV ..."
+echo "deploy new branch: $BRANCH, ENV: $DEPLOY_ENV, REV: $DEPLOY_REV ..."
 cd "$SOURCE_PATH/bbs15eng/upload"
 sudo cp -r * "$TARGET_PATH/" 
 cd "$SOURCE_PATH"
 
 echo "start Apache..."
-sudo /etc/init.d/httpd start
+if [ "$DEPLOY_ENV" == "prod" ];then
+  sudo /etc/init.d/httpd start
+fi
 
 echo "Done. Good Luck!"
